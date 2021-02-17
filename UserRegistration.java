14d138e9773732325f8c432e80d48273b43bf825
kaushiki <kaushiki.ak88@gmail.com>
2021-02-17 19:26:23 +0530

    Validation of Users LastName

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,7 +4,8 @@ import java.util.regex.*;
 
 public class UserRegistration{
 
-	public static void firstName(){
+	// Checking the validation of First name
+	public static void firstNameValidation(){
 		Scanner Obj = new Scanner(System.in);
       System.out.println("Enter your First name: ");
       String name = Obj.nextLine();
@@ -28,9 +29,38 @@ public class UserRegistration{
             System.out.println("Not valid first name should starts with Cap and has minimum 3 character ");
 	       }
    }
-	public static void main(String[] args){
-		UserRegistration user = new UserRegistration();
-		user.firstName();
+
+	// Checking the validation of last name
+	 public static void lastNameValidation(){
+      Scanner Obj = new Scanner(System.in);
+      System.out.println("Enter your Last name: ");
+      String name = Obj.nextLine();
+
+      //Checking the entered name
+      String regex = "^[A-Z][a-z]{2,}$" ;
+
+      //Object pattern created
+      Pattern pattern = Pattern.compile(regex);
+
+      // Creating a Matcher object
+      Matcher matcher = pattern.matcher(name);
+
+      boolean found = matcher.matches();
+
+         //checking if patter matched or not
+         if ( found == true ) {
+            System.out.println("Last name is valid");
+         }
+         else {
+            System.out.println("Not valid first name should starts with Cap and has minimum 3 character ");
+          }
+   }
+   public static void main(String[] args){
+
+		UserRegistration firstUser = new UserRegistration();
+      firstUser.firstNameValidation();
+		firstUser.lastNameValidation();
+
  	}
 }
 
