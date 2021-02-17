2bbee36cfb6094de3783af267892ccdbdc167af3
kaushiki <kaushiki.ak88@gmail.com>
2021-02-17 20:17:37 +0530

    Verifying the Valide Mobile number

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.*;
 
-public class UserRegistration{
-
+public class UserRegistration
+{
 	static Scanner Obj = new Scanner(System.in);
 
 	// Checking the validation of First name
-	public static void firstNameValidation(){
-
+	public static void firstNameValidation()
+{
       System.out.println("Enter your First name: ");
       String name = Obj.nextLine();
 
@@ -24,17 +24,19 @@ public class UserRegistration{
 		boolean found = matcher.matches();
 
          //checking if patter matched or not
-         if ( found == true ) {
+         if ( found == true )
+			{
             System.out.println("First name is valid");
          }
-         else {
-            System.out.println("Not valid first name should starts with Cap and has minimum 3 character ");
-	       }
+         else
+			{
+         	System.out.println("Not valid first name should starts with Cap and has minimum 3 character ");
+	      }
    }
 
 	// Checking the validation of last name
-	 public static void lastNameValidation(){
-
+	public static void lastNameValidation()
+	{
       System.out.println("Enter your Last name: ");
       String name = Obj.nextLine();
 
@@ -50,17 +52,19 @@ public class UserRegistration{
       boolean found = matcher.matches();
 
          //checking if patter matched or not
-         if ( found == true ) {
+         if ( found == true )
+			{
             System.out.println("Last name is valid");
          }
-         else {
+         else
+			{
             System.out.println("Not valid first name should starts with Cap and has minimum 3 character ");
           }
    }
 
 	//Cheking the Validation of Email
-	public static void emailValidation() {
-
+	public static void emailValidation() 
+	{
 		System.out.println("Enter your email id: ");
 		String name = Obj.nextLine();
 
@@ -74,20 +78,49 @@ public class UserRegistration{
 
 		// Verifying whether given phone number is valid
 		boolean result = matcher.matches();
-		if (result == true) {
+		if (result == true)
+		{
 			System.out.println(" Email is valid");
-		} else {
-			System.out.println("Not valid");
+		}
+		else
+		{
 			System.out.println("Email id should be in Eg,: abc.def123@gmail.com ");
 		}
 	}
 
-   public static void main(String[] args){
+	//Checking for the valide mobile number
+	public static void mobileValidation()
+	{
+		System.out.println("Enter your mobile number: ");
+		String name = Obj.nextLine();
 
+		String regex = "[91]+[ ]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]+[0-9]";
+
+		//Creating a pattern object
+		Pattern pattern = Pattern.compile(regex);
+
+		// Creating a Matcher object
+		Matcher matcher = pattern.matcher(name);
+
+		// Verifying whether given phone number is valid
+		boolean result = matcher.matches();
+		if (result == true)
+		{
+			System.out.println("Mobile number is  valid");
+		}
+		else
+		{
+			System.out.println("Mobile number formate should be ex:91 1234567890 ");
+		}
+	}
+
+   public static void main(String[] args)
+	{
 		UserRegistration User = new UserRegistration();
       User.firstNameValidation();
 		User.lastNameValidation();
 		User.emailValidation();
- 	}
+ 		User.mobileValidation();
+	}
 }
 
